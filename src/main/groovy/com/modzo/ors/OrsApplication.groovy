b3599cacf0c8c46d578ef0fc04fef3ece76f4fce
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-01-23 23:04:56 +0200

    Initial commit

---
+++ src/main/groovy/com/modzo/ors/OrsApplication.groovy
@@ -0,0 +1,12 @@
+package com.modzo.ors
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class OrsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run OrsApplication, args
+	}
+}
