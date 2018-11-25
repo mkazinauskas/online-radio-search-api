934c5d1704495ab7c62976825c3fbeb499c351d8
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-11-25 23:16:57 +0200

    Song search functionality. Endpoint test missing

---
+++ api/src/main/java/com/mozdzo/ors/search/resources/song/SongResultResource.java
@@ -0,0 +1,38 @@
+package com.mozdzo.ors.search.resources.song;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mozdzo.ors.search.domain.SongDocument;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.ResourceSupport;
+
+import java.util.Map;
+
+import static java.util.Collections.singletonMap;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+public class SongResultResource extends ResourceSupport {
+
+    private final SongResultResponse song;
+
+    @JsonCreator
+    private SongResultResource(@JsonProperty("song") SongResultResponse song,
+                               @JsonProperty("_links") Map<String, Link> links) {
+        this.song = song;
+        links.forEach((label, link) -> add(link.withRel(label)));
+    }
+
+    static SongResultResource create(SongDocument song) {
+        SongResultResponse response = SongResultResponse.create(song);
+        Link link = linkTo(methodOn(SongSearchController.class)
+                .search("", Pageable.unpaged()))
+                .withSelfRel();
+        return new SongResultResource(response, singletonMap(link.getRel(), link));
+    }
+
+    public SongResultResponse getSong() {
+        return song;
+    }
+}
