922297b98018999aa0de570bb9031fe1eb3c7d84
modestas.kazinauskas <m.kazinauskas@gmail.com>
2020-01-27 00:33:14 +0200

    Initial page look

--- dynamic-web/src/App.js
+++ dynamic-web/src/App.js
@@ -1,19 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Menu, Icon } from 'antd';
+
+const { Header, Content, Sider, Footer } = Layout;
 
 class App extends Component {
+
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+            <Menu style={{ float: 'right', lineHeight: '64px' }} theme="light" mode="horizontal">
+              <Menu.Item key="1">
+                <Icon type="user" />
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="video-camera" />
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="upload" />
+              </Menu.Item>
+            </Menu>
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              background: '#fff',
+              minHeight: 280,
+            }}
+          >
+            Content
+        </Content>
+          <Footer>Test</Footer>
+        </Layout>
+
+      </Layout>
     );
   }
 }
