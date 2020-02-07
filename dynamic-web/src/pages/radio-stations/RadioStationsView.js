27d879925b2698581cb0d094ce818dc565fe493c
modestas.kazinauskas <m.kazinauskas@gmail.com>
2020-02-07 21:49:13 +0200

    keycloak token print

--- dynamic-web/src/pages/radio-stations/RadioStationsView.js
+++ dynamic-web/src/pages/radio-stations/RadioStationsView.js
@@ -25,18 +25,25 @@ class RadioStationsView extends Component {
             .then(authenticated => {
                 console.log('init');
                 console.log(authenticated);
+                console.log(keycloak);
                 this.setState({ keycloak: keycloak, authenticated: authenticated })
             });
     }
 
     render() {
         if (this.state.keycloak) {
-            if (this.state.authenticated) return (
-                <div>
-                    <p>This is a Keycloak-secured component of your application. You shouldn't be able
+            if (this.state.authenticated) {
+                return (
+                    <div>
+                        {this.state.keycloak.token}
+
+                        <p>This is a Keycloak-secured component of your application. You shouldn't be able
                 to see this unless you've authenticated with Keycloak.</p>
-                </div>
-            ); else return (<div>Unable to authenticate!</div>)
+                    </div>
+                )
+            } else {
+                return (<div>Unable to authenticate!</div>);
+            }
         }
         return (
             <div>Initializing Keycloak...</div>
