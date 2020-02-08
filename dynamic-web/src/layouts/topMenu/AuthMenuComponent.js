6b82e87564e8d07461a37ae12c8971f10db427ac
modestas.kazinauskas <m.kazinauskas@gmail.com>
2020-02-08 15:18:15 +0200

    Keycloak authorized users menu initial refactor

---
+++ dynamic-web/src/layouts/topMenu/AuthMenuComponent.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+import { signIn, signOut } from '../../auth/actions';
+import { connect } from 'react-redux';
+import { Menu, Icon } from 'antd';
+
+class AuthMenuComponent extends Component {
+
+    wrapWithMenu(items) {
+        return (
+            <Menu style={{ float: 'right', lineHeight: '64px' }} theme="light" mode="horizontal">
+                {items}
+            </Menu>
+        );
+    }
+
+    renderLoadingUser() {
+        return this.wrapWithMenu(
+            <Menu.Item key={0}>
+                <Icon type="user" />
+                <span>Loading...</span>
+            </Menu.Item>
+        );
+    }
+
+    renderAuthorizedUser() {
+        return this.wrapWithMenu(
+            <Menu.Item key={0} onClick={this.props.signOut}>
+                <Icon type="user" />
+                <span>Logout</span>
+            </Menu.Item>
+        );
+    }
+
+    renderUnauthorizedUser() {
+        return this.wrapWithMenu(
+            <Menu.Item key={0} onClick={this.props.signIn}>
+                <Icon type="user" />
+                <span>Login</span>
+            </Menu.Item>
+        );
+    }
+
+    render() {
+        if (this.props.loading) {
+            return this.renderLoadingUser();
+        }
+        if (this.props.authenticated) {
+            return this.renderAuthorizedUser();
+        }
+        return this.renderUnauthorizedUser();
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        loading: state.auth.loading,
+        authenticated: state.auth.authenticated
+    }
+}
+
+export default connect(mapStateToProps, { signIn, signOut })(AuthMenuComponent);
--- dynamic-web/src/layouts/topMenu/TopMenuComponent.js
+++ dynamic-web/src/layouts/topMenu/TopMenuComponent.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import AuthButtonComponent from './AuthButtonComponent';
-import { Menu, Icon } from 'antd';
+import AuthButtonComponent from './AuthMenuComponent';
 
 export default () => (
-    <Menu style={{ float: 'right', lineHeight: '64px' }} theme="light" mode="horizontal">
-        <Menu.Item key='1' onClick={()=>alert('Go')}>
-            <Icon type="user" />
-            <AuthButtonComponent />
-        </Menu.Item>
-    </Menu>
+        <AuthButtonComponent/>
 );;
