a4937392b76e6992d0ed7d949b20d6ee95312673
modestas.kazinauskas <m.kazinauskas@gmail.com>
2020-02-08 15:58:26 +0200

    Footer displays full year

--- dynamic-web/src/layouts/FooterComponent.js
+++ dynamic-web/src/layouts/FooterComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Layout } from 'antd';
 
+const year = new Date().getUTCFullYear();
+
 export default () => (
-    <Layout.Footer style={{ textAlign: 'center' }}> Online Radio Search ©2019</Layout.Footer>
+    <Layout.Footer style={{ textAlign: 'center' }}> Online Radio Search ©{year}</Layout.Footer>
 );
