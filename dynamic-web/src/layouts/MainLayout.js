6692d1d8e195617372a7dcdb76f31ba95db8f9ba
modestas.kazinauskas <m.kazinauskas@gmail.com>
2020-02-09 22:14:41 +0200

    Loading application sate

--- dynamic-web/src/layouts/MainLayout.js
+++ dynamic-web/src/layouts/MainLayout.js
@@ -1,45 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
-import { Layout, Icon } from 'antd';
-import LeftSideMenuComponent from './LeftSideMenuComponent';
-import TopMenuComponent from './topMenu/TopMenuComponent';
-import ContentComponent from './ContentComponent';
-import FooterComponent from './FooterComponent';
-import './MainLayout.css';
+import LoadingApplicationLayout from './LoadingApplicationLayout';
+import DefaultApplicationLayout from './DefaultApplicationLayout';
+import { connect } from 'react-redux';
 
 class MainLayout extends Component {
 
-  state = {
-    collapsed: false,
-  };
-
-  toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  };
-
   render() {
-    return (
-      <Router>
-        <Layout>
-          <LeftSideMenuComponent collapsed={this.state.collapsed} />
-          <Layout>
-            <Layout.Header style={{ background: '#fff', padding: 0 }}>
-              <Icon
-                className="trigger"
-                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                onClick={this.toggle}
-              />
-              <TopMenuComponent />
-            </Layout.Header>
-            <ContentComponent />
-            <FooterComponent />
-          </Layout>
-        </Layout>
-      </Router>
-    );
+    if (this.props.loading) {
+      return (<LoadingApplicationLayout />);
+    } else {
+      return (<DefaultApplicationLayout />);
+    }
   }
 }
 
-export default MainLayout;
+const mapStateToProps = (state) => {
+  return {
+    loading: state.auth.loading,
+  }
+}
+
+export default connect(mapStateToProps)(MainLayout);
