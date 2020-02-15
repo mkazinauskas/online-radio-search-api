878476253034631572ad275c5964503c7ff9c2f1
modestas.kazinauskas <m.kazinauskas@gmail.com>
2020-02-15 23:26:37 +0200

    Initial radio station streams implementation

--- dynamic-web/src/layouts/pathTypes.js
+++ dynamic-web/src/layouts/pathTypes.js
@@ -1,3 +1,8 @@
 export const HOME = '/';
 export const RADIO_STATIONS = '/radio-stations';
 export const SONGS = '/songs';
+export const RADIO_STATION_STREAMS = '/radio-stations/:radioStationId/streams';
+
+export const createURLRadioStationStreams = (radioStationId) =>{
+    return RADIO_STATION_STREAMS.replace(':radioStationId', radioStationId);
+}
