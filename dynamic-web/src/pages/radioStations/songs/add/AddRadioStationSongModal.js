afb2754e97caa6fa1bf9ed373d3b94790094eb33
modestas.kazinauskas <m.kazinauskas@gmail.com>
2020-02-17 23:26:09 +0200

    Radio station addition

--- dynamic-web/src/pages/radioStations/songs/add/AddRadioStationSongModal.js
+++ dynamic-web/src/pages/radioStations/songs/add/AddRadioStationSongModal.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
-import { Modal, Form, Input, Alert } from 'antd';
+import { Alert, DatePicker, Form, Input, Modal, TimePicker } from 'antd';
 import Axios from 'axios';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { isWebUri } from 'valid-url';
 
 const DEFAULT_STATE = {
     loading: false,
@@ -28,15 +27,24 @@ class AddRadioStationSongModal extends Component {
                     }
                 }
 
+                const mergedDateWithTime = values.date;
+                mergedDateWithTime.set({
+                    hour: values.time.get('hour'),
+                    minute: values.time.get('minute'),
+                    second: values.time.get('second'),
+                    mili: values.time.get('second')
+                });
+                const playedTime = mergedDateWithTime.format('YYYY-MM-DDTHH:mm:ssZ');
                 const content = {
-                    ...values
+                    songId: values.songId,
+                    playedTime
                 }
 
                 const radioStationId = this.props.match.params.radioStationId;
 
-                Axios.post(`/admin/radio-stations/${radioStationId}/streams`, content, config)
-                    .then(() => this.setState({ ...this.state, successMessage: 'Radio station stream was added' }))
-                    .catch(() => this.setState({ ...this.state, errorMessage: 'Failed to add stream' }))
+                Axios.post(`/admin/radio-stations/${radioStationId}/songs`, content, config)
+                    .then(() => this.setState({ ...this.state, successMessage: 'Radio station song was added' }))
+                    .catch(() => this.setState({ ...this.state, errorMessage: 'Failed to add played song' }))
                     .then(() => this.setState({ ...this.state, loading: false }));
             }
         });
@@ -52,33 +60,22 @@ class AddRadioStationSongModal extends Component {
 
         const form = (
             <Form labelCol={{ span: 5 }} wrapperCol={{ span: 12 }} onSubmit={this.handleSubmit}>
-                <Form.Item label="URL">
-                    {getFieldDecorator('url', {
+                <Form.Item label="Song Id">
+                    {getFieldDecorator('songId', {
                         rules: [
-                            { required: true, message: 'Please input radio station stream title!' },
-                            {
-                                validator: (rule, value, callback) => {
-                                    if (value === value.trim()) {
-                                        callback();
-                                    } else {
-                                        callback(false);
-                                    }
-                                },
-                                message: 'Value cannot have empty spaces around',
-                            },
-                            {
-                                validator: (rule, value, callback) => {
-                                    if (isWebUri(value)) {
-                                        callback();
-                                    } else {
-                                        callback(false);
-                                    }
-                                },
-                                message: 'Radio station stream url is invalid',
-                            }
-
+                            { required: true, message: 'Please input song id!' },
                         ],
-                    })(<Input />)}
+                    })(<Input type="number" />)}
+                </Form.Item>
+                <Form.Item label="Date">
+                    {getFieldDecorator('date', {
+                        rules: [{ type: 'object', required: true, message: 'Please select date!' }],
+                    })(<DatePicker />)}
+                </Form.Item>
+                <Form.Item label="Time">
+                    {getFieldDecorator('time', {
+                        rules: [{ type: 'object', required: true, message: 'Please select time!' }],
+                    })(<TimePicker />)}
                 </Form.Item>
             </Form>
         );
@@ -93,7 +90,7 @@ class AddRadioStationSongModal extends Component {
         return (
             <span>
                 <Modal
-                    title="Add New Radio Station Stream"
+                    title="Add New Radio Station Song"
                     visible={this.props.visible}
                     okText="Add"
                     okButtonProps={{ disabled: this.state.loading }}
