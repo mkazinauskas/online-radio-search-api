20530ac275ca31a6eef1bbddc5def8dd1891b1b9
modestas.kazinauskas <m.kazinauskas@gmail.com>
2020-02-20 00:22:24 +0200

    Website reader headers update

--- api/src/main/java/com/modzo/ors/stations/services/stream/scrapper/WebPageReader.java
+++ api/src/main/java/com/modzo/ors/stations/services/stream/scrapper/WebPageReader.java
@@ -3,9 +3,7 @@ package com.modzo.ors.stations.services.stream.scrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,6 +15,8 @@ import java.util.Set;
 public class WebPageReader {
 
     private static final Logger log = LoggerFactory.getLogger(WebPageReader.class);
+
+    private static final HttpEntity<String> DEFAULT_REQUEST_HEADERS = headers();
 
     private static final Set<MediaType> ALLOWED_MEDIA_TYPES = Set.of(
             MediaType.TEXT_PLAIN,
@@ -38,7 +38,10 @@ public class WebPageReader {
     public Optional<String> read(String url) {
         try {
             log.info("Reading url = `{}`", url);
-            HttpHeaders httpHeaders = restTemplate.headForHeaders(url, String.class);
+            HttpHeaders httpHeaders = restTemplate
+                    .exchange(url, HttpMethod.HEAD, DEFAULT_REQUEST_HEADERS, String.class)
+                    .getHeaders();
+
             MediaType contentType = httpHeaders.getContentType();
             if (contentType == null) {
                 log.info("Content type of url = `{}` was null", url);
@@ -46,7 +49,9 @@ public class WebPageReader {
             }
             if (ALLOWED_MEDIA_TYPES.stream().anyMatch(contentType::isCompatibleWith)) {
                 log.info("Successfully finished reading url = `{}`", url);
-                ResponseEntity<String> forEntity = restTemplate.getForEntity(url, String.class);
+                ResponseEntity<String> forEntity = restTemplate
+                        .exchange(url, HttpMethod.GET, DEFAULT_REQUEST_HEADERS, String.class);
+
                 return Optional.ofNullable(forEntity.getBody());
             } else {
                 log.info("Content type `{}` is not readable of url = `{}`", contentType.getType(), url);
@@ -56,5 +61,12 @@ public class WebPageReader {
             log.error(String.format("Failed to read url = `%s`", url), exception);
             return Optional.empty();
         }
+    }
+
+    private static HttpEntity<String> headers() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 " +
+                "(KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36");
+        return new HttpEntity<>(headers);
     }
 }
