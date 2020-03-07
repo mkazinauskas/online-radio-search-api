94f2bcd8d00ba0f17af4e40a94587a848595b5db
modestas.kazinauskas <m.kazinauskas@gmail.com>
2020-03-08 00:36:50 +0200

    Radio station streams refactor

--- static-web/src/main/java/com/modzo/ors/web/web/radio/stations/RadioStationStreamService.java
+++ static-web/src/main/java/com/modzo/ors/web/web/radio/stations/RadioStationStreamService.java
@@ -2,78 +2,33 @@ package com.modzo.ors.web.web.radio.stations;
 
 import com.modzo.ors.web.web.api.radio.stations.streams.RadioStationStreamResponse;
 import com.modzo.ors.web.web.api.radio.stations.streams.RadioStationStreamsClient;
+import com.modzo.ors.web.web.components.common.Paged;
+import com.modzo.ors.web.web.components.common.model.RadioStationStreamModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 class RadioStationStreamService {
+
     private final RadioStationStreamsClient client;
 
     public RadioStationStreamService(RadioStationStreamsClient client) {
         this.client = client;
     }
 
-    List<Data> retrieve(Long id) {
+    Paged<RadioStationStreamModel> retrieve(Long id) {
         PagedModel<EntityModel<RadioStationStreamResponse>> streams = client.getRadioStationStreams(id);
-        return streams.getContent()
+        var content = streams.getContent()
                 .stream()
                 .map(EntityModel::getContent)
-                .map(response -> new Data(
-                        response.getId(),
-                        response.getUniqueId(),
-                        response.getUrl(),
-                        response.getBitRate(),
-                        response.getType()
-                ))
+                .map(RadioStationStreamModel::new)
                 .collect(Collectors.toList());
-    }
-
-    public static class Data {
-
-        private final long id;
-
-        private final String uniqueId;
-
-        private final String url;
-
-        private final Integer bitRate;
-
-        private final String type;
-
-        private Data(long id,
-                     String uniqueId,
-                     String url,
-                     Integer bitRate,
-                     String type) {
-            this.id = id;
-            this.uniqueId = uniqueId;
-            this.url = url;
-            this.bitRate = bitRate;
-            this.type = type;
-        }
-
-        public long getId() {
-            return id;
-        }
-
-        public String getUniqueId() {
-            return uniqueId;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public int getBitRate() {
-            return bitRate;
-        }
-
-        public String getType() {
-            return type;
-        }
+        return new Paged<>(
+                content,
+                streams.getMetadata()
+        );
     }
 }
