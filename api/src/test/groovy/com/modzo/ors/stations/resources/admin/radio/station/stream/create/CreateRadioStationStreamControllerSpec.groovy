3738dc57f52f6abf8cc608dd7bce4166fb2f24f1
modestas.kazinauskas <m.kazinauskas@gmail.com>
2020-03-11 18:52:40 +0200

    Update radio station streams

--- api/src/test/groovy/com/modzo/ors/stations/resources/admin/radio/station/stream/create/CreateRadioStationStreamControllerSpec.groovy
+++ api/src/test/groovy/com/modzo/ors/stations/resources/admin/radio/station/stream/create/CreateRadioStationStreamControllerSpec.groovy
@@ -4,10 +4,8 @@ import com.modzo.ors.HttpEntityBuilder
 import com.modzo.ors.TestUsers
 import com.modzo.ors.stations.domain.radio.station.RadioStation
 import com.modzo.ors.stations.resources.IntegrationSpec
-import com.modzo.ors.stations.resources.admin.radio.station.stream.create.CreateRadioStationStreamRequest
 import org.springframework.http.ResponseEntity
 
-import static com.modzo.ors.TestUsers.ADMIN
 import static org.springframework.http.HttpMethod.POST
 import static org.springframework.http.HttpStatus.OK
 import static org.testcontainers.shaded.org.apache.commons.lang.RandomStringUtils.randomAlphanumeric
