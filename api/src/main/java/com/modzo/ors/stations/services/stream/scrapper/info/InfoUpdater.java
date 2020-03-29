b858c952322c4df036de328c622c7d339d54f236
modestas.kazinauskas <m.kazinauskas@gmail.com>
2020-03-29 22:20:24 +0300

    Code refactor

--- api/src/main/java/com/modzo/ors/stations/services/stream/scrapper/info/InfoUpdater.java
+++ api/src/main/java/com/modzo/ors/stations/services/stream/scrapper/info/InfoUpdater.java
@@ -1,32 +1,30 @@
 package com.modzo.ors.stations.services.stream.scrapper.info;
 
 import com.modzo.ors.stations.domain.radio.station.stream.StreamUrl;
-import com.modzo.ors.stations.domain.radio.station.stream.StreamUrls;
+import com.modzo.ors.stations.domain.radio.station.stream.commands.FindOldestCheckedRadioStationStreamUrl;
 import org.springframework.stereotype.Component;
 
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Optional;
 
 @Component
 class InfoUpdater {
 
     private final InfoUpdaterService infoUpdaterService;
 
-    private final StreamUrls streamUrls;
+    private final FindOldestCheckedRadioStationStreamUrl.Handler findOldestCheckedRadioStationStreamUrlHandler;
 
     InfoUpdater(InfoUpdaterService infoUpdaterService,
-                StreamUrls streamUrls) {
+                FindOldestCheckedRadioStationStreamUrl.Handler findOldestCheckedRadioStationStreamUrlHandler) {
         this.infoUpdaterService = infoUpdaterService;
-        this.streamUrls = streamUrls;
+        this.findOldestCheckedRadioStationStreamUrlHandler = findOldestCheckedRadioStationStreamUrlHandler;
     }
 
     void update() {
         ZonedDateTime before = ZonedDateTime.now().minus(5, ChronoUnit.DAYS);
 
-        Optional<StreamUrl> stream = streamUrls
-                .findTop1ByTypeAndCheckedBeforeOrCheckedIsNull(StreamUrl.Type.INFO, before);
-
-        stream.ifPresent(it -> infoUpdaterService.update(it.getStream().getRadioStationId(), it.getId()));
+        findOldestCheckedRadioStationStreamUrlHandler.handle(
+                new FindOldestCheckedRadioStationStreamUrl(StreamUrl.Type.INFO, before)
+        ).ifPresent(it -> infoUpdaterService.update(it.getStream().getRadioStationId(), it.getId()));
     }
 }
--- api/src/main/java/com/modzo/ors/stations/services/stream/scrapper/songs/SongsUpdater.java
+++ api/src/main/java/com/modzo/ors/stations/services/stream/scrapper/songs/SongsUpdater.java
@@ -1,34 +1,30 @@
 package com.modzo.ors.stations.services.stream.scrapper.songs;
 
-import com.modzo.ors.stations.domain.radio.station.stream.RadioStationStream;
-import com.modzo.ors.stations.domain.radio.station.stream.RadioStationStreams;
 import com.modzo.ors.stations.domain.radio.station.stream.StreamUrl;
-import com.modzo.ors.stations.domain.radio.station.stream.StreamUrls;
+import com.modzo.ors.stations.domain.radio.station.stream.commands.FindOldestCheckedRadioStationStreamUrl;
 import org.springframework.stereotype.Component;
 
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Optional;
 
 @Component
 class SongsUpdater {
 
-    private final StreamUrls streamUrls;
+    private final FindOldestCheckedRadioStationStreamUrl.Handler findOldestCheckedRadioStationStreamUrlHandler;
 
     private final SongsUpdaterService updaterService;
 
-    private SongsUpdater(StreamUrls streamUrls,
+    private SongsUpdater(FindOldestCheckedRadioStationStreamUrl.Handler findOldestCheckedRadioStationStreamUrlHandler,
                          SongsUpdaterService updaterService) {
-        this.streamUrls = streamUrls;
+        this.findOldestCheckedRadioStationStreamUrlHandler = findOldestCheckedRadioStationStreamUrlHandler;
         this.updaterService = updaterService;
     }
 
     void update() {
         ZonedDateTime before = ZonedDateTime.now().minus(1, ChronoUnit.HOURS);
 
-        Optional<StreamUrl> stream = streamUrls
-                .findTop1ByTypeAndCheckedBeforeOrCheckedIsNull(StreamUrl.Type.SONGS, before);
-
-        stream.ifPresent(it -> updaterService.update(it.getStream().getRadioStationId(), it.getId()));
+        findOldestCheckedRadioStationStreamUrlHandler.handle(
+                new FindOldestCheckedRadioStationStreamUrl(StreamUrl.Type.SONGS, before)
+        ).ifPresent(it -> updaterService.update(it.getStream().getRadioStationId(), it.getId()));
     }
 }
