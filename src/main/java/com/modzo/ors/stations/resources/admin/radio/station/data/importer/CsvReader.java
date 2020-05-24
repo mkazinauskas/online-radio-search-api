b92c4a1b85d2fa7d1f4ef21edea89e183b7b5ed1
modestas.kazinauskas <modestas.kazinauskas@gmail.com>
2020-05-24 23:45:57 +0300

    Csv read and write check

--- src/main/java/com/modzo/ors/stations/resources/admin/radio/station/data/importer/CsvReader.java
+++ src/main/java/com/modzo/ors/stations/resources/admin/radio/station/data/importer/CsvReader.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import com.modzo.ors.stations.resources.admin.radio.station.data.CsvData;
 import com.modzo.ors.stations.resources.admin.radio.station.data.CsvMapperConfiguration;
 import com.rainerhahnekamp.sneakythrow.Sneaky;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -17,8 +16,8 @@ class CsvReader {
 
     static List<CsvData> read(MultipartFile file) {
         InputStream input = Sneaky.sneak(file::getInputStream);
-        MappingIterator<CsvData> readValues = Sneaky.sneak(() -> READER.readValues(input));
-        return Sneaky.sneak(readValues::readAll);
+        MappingIterator<CsvData> lines = Sneaky.sneak(() -> READER.readValues(input));
+        return Sneaky.sneak(lines::readAll);
     }
 
 }
---
+++ src/test/groovy/com/modzo/ors/stations/resources/admin/radio/station/data/CsvMapperConfigurationSpec.groovy
@@ -0,0 +1,39 @@
+package com.modzo.ors.stations.resources.admin.radio.station.data
+
+import com.fasterxml.jackson.databind.ObjectReader
+import com.fasterxml.jackson.databind.ObjectWriter
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class CsvMapperConfigurationSpec extends Specification {
+
+    @Shared
+    ObjectWriter WRITER = CsvMapperConfiguration.constructWriter()
+
+    @Shared
+    ObjectReader READER = CsvMapperConfiguration.constructReader()
+
+    @Unroll
+    void 'should read created csv file'() {
+        given:
+            CsvData data = new CsvData(radioStationName: radioStationName, streamUrl: streamUrl)
+            ByteArrayOutputStream output = new ByteArrayOutputStream()
+        when:
+            WRITER.writeValue(output, [data])
+        then:
+            output.toByteArray().length > 0
+        when:
+            List<CsvData> result = READER.readValues(output.toByteArray()).readAll()
+        then:
+            result.size() == 1
+            with(result.first()) { entry ->
+                entry.radioStationName == radioStationName
+                entry.streamUrl == streamUrl
+            }
+        where:
+            radioStationName            | streamUrl
+            'Simple radio station name' | 'https://test.com/mp3'
+            '!@#$%^&*()_,.,"'           | '!@#$%^&*()_,.;"'
+    }
+}
