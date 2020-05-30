0533315a8dade887f54cb95a1f4d4d4111b197b1
modestas.kazinauskas <modestas.kazinauskas@gmail.com>
2020-05-31 01:05:40 +0300

    Url check timeouts update

--- src/main/java/com/modzo/ors/stations/services/stream/WebPageReader.java
+++ src/main/java/com/modzo/ors/stations/services/stream/WebPageReader.java
@@ -44,8 +44,8 @@ public class WebPageReader {
         try {
             URL parsedUrl = new URL(url);
             connection = (HttpURLConnection) parsedUrl.openConnection();
-            connection.setConnectTimeout(30);
-            connection.setReadTimeout(30);
+            connection.setConnectTimeout(30000);
+            connection.setReadTimeout(30000);
             connection.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
                     + "(KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36");
             connection.setInstanceFollowRedirects(false);
