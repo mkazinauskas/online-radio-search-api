043308a519cc4b775340bfe62a06136b5fac6435
Modestas <m.kazinauskas@gmail.com>
2021-04-26 21:45:13 +0300

    Initial configuration

--- src/main/java/com/modzo/ors/Application.java
+++ src/main/java/com/modzo/ors/Application.java
@@ -2,13 +2,17 @@ package com.modzo.ors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = ElasticsearchDataAutoConfiguration.class
+)
 @EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
---
+++ src/main/java/com/modzo/ors/configuration/elastic/DefaultElasticsearchConfiguration.java
@@ -0,0 +1,43 @@
+package com.modzo.ors.configuration.elastic;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class DefaultElasticsearchConfiguration extends AbstractElasticsearchConfiguration {
+
+    @Value("${elasticsearch.host}")
+    private String elasticSearchHost;
+
+    @Value("${elasticsearch.port}")
+    private int elasticSearchPort;
+
+    @Value("${elasticsearch.clustername}")
+    private String elasticSearchClusterName;
+
+    @Bean
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(String.format("%s:%s", elasticSearchHost, elasticSearchPort))
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+    @Bean(name = {"elasticsearchOperations", "elasticsearchTemplate"})
+    public ElasticsearchRestTemplate defaultElasticSearchTemplate(RestHighLevelClient client) {
+        return new ElasticsearchRestTemplate(client);
+    }
+
+}
