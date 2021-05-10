bec9d1e0ef190ca779195ef593185eb06f9adcf6
Modestas <m.kazinauskas@gmail.com>
2021-05-11 00:00:02 +0300

    Elastic search template naming fix

--- src/main/java/com/modzo/ors/configuration/elastic/DefaultElasticsearchConfiguration.java
+++ src/main/java/com/modzo/ors/configuration/elastic/DefaultElasticsearchConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
@@ -30,7 +29,7 @@ public class DefaultElasticsearchConfiguration extends AbstractElasticsearchConf
     }
 
     @Bean
-    public ElasticsearchOperations elasticsearchTemplate() {
+    public ElasticsearchRestTemplate customElasticsearchTemplate() {
         return new ElasticsearchRestTemplate(elasticsearchClient());
     }
 
